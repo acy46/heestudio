43c0470c7a7aa06682dbeaee2890ff921481f7f7
acy46 <helenacy46@gmail.com>
2026-08-29 18:15:07 +0200

    Delete static-site directory

--- static-site/assets/site.js
+++
@@ -1,3 +0,0 @@
-document.querySelectorAll('.project-quote').forEach((quote) => {
-  quote.innerHTML = quote.textContent.trim().split(/\s+/).map((word) => `<span class="word">${word}</span>`).join(' ');
-});
